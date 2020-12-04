b11ed0ad1cff9eaf636a61f993b3a1a79d3cc20a
Cody Crawmer <codercodyc@gmail.com>
2020-12-03 18:28:30 -0800

    line charts formatted

--- Squab-Pickup-Tracker/Controller/Move-Cull/MoveCullTableViewController.swift
+++ Squab-Pickup-Tracker/Controller/Move-Cull/MoveCullTableViewController.swift
@@ -22,8 +22,12 @@ class MoveCullTableViewController: UITableViewController {
         
     }
     
-    func updateGraph() {
-        
+    func sumData(data: [Double]) -> Double {
+        var sum: Double = 0
+        for item in data {
+            sum += item
+        }
+        return sum
     }
 
     // MARK: - Table view data source
@@ -42,37 +46,70 @@ class MoveCullTableViewController: UITableViewController {
         if let safeCell = tableView.dequeueReusableCell(withIdentifier: K.ProductionByWeekCellIdentifier, for: indexPath) as? ProductionByPairTableViewCell {
             safeCell.penNestLabel.text = "40\(indexPath.row)-\(indexPath.row + 1)A"
             var randomData: [Double] = []
-            for i in 0...11 {
-                let number = Double(Int.random(in: 0...4))
+//            var totals: [Double] = []
+            for _ in 0...11 {
+                let number = Double(Int.random(in: 0...2))
                 randomData.append(number)
+//
+//                let newTotal = sumData(data: randomData)
+//                totals.append(newTotal)
             }
             
             var lineChartEntry = [ChartDataEntry]()
+//            var totalsEntry = [ChartDataEntry]()
+            
             
             for i in 0..<randomData.count {
                 let value = ChartDataEntry(x: Double(i), y: randomData[i])
                 lineChartEntry.append(value)
+                
+//                let total = ChartDataEntry(x: Double(i), y: totals[i])
+//                totalsEntry.append(total)
             }
             
+            
             let line1 = LineChartDataSet(entries: lineChartEntry)
-            line1.colors = [NSUIColor.blue]
+            line1.colors = [#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)]
             line1.drawFilledEnabled = true
-//            line1.drawCircleHoleEnabled = false
-            line1.circleRadius = 5
+            line1.circleRadius = 4.5
             line1.drawValuesEnabled = false
             
-            let data = LineChartData(dataSet: line1)
+            let gradientColors = [UIColor.blue.cgColor, UIColor.clear.cgColor] as CFArray // Colors of the gradient
+            let colorLocations:[CGFloat] = [0.75, 0.0] // Positioning of the gradient
+            let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) // Gradient Object
+            line1.fill = Fill.fillWithLinearGradient(gradient!, angle: 90.0) // Set the Gradient
+            line1.drawFilledEnabled = true // Draw the Gradient
+            
+//            let line2 = LineChartDataSet(entries: totalsEntry)
+//            line2.colors = [NSUIColor.red]
+//            line2.circleRadius = 2
+//            line2.drawValuesEnabled = false
+            
+            let data = LineChartData(dataSets: [line1])
             safeCell.chart.data = data
             
             safeCell.chart.isUserInteractionEnabled = false
             
+            
+            safeCell.chart.animate(xAxisDuration: 1.0)
+            
+            
             safeCell.chart.legend.enabled = false
-//            safeCell.chart.leftAxis.drawLabelsEnabled = false
-//            safeCell.chart.leftAxis.drawZeroLineEnabled = true
+            safeCell.chart.leftAxis.axisMaximum = 2.25
+            
             safeCell.chart.leftAxis.granularity = 1
             safeCell.chart.rightAxis.granularity = 1
             safeCell.chart.leftAxis.axisMinimum = 0
-            safeCell.chart.rightAxis.axisMinimum = 0
+
+            let limit = ChartLimitLine(limit: 0.67)
+            limit.lineColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+            limit.lineWidth = 1
+            safeCell.chart.leftAxis.addLimitLine(limit)
+            
+            safeCell.chart.leftAxis.drawZeroLineEnabled = true
+            safeCell.chart.leftAxis.drawLimitLinesBehindDataEnabled = true
+            safeCell.chart.leftAxis.drawGridLinesEnabled = false
+            safeCell.chart.leftAxis.drawAxisLineEnabled = false
             safeCell.chart.rightAxis.drawLabelsEnabled = false
             safeCell.chart.rightAxis.drawGridLinesEnabled = false
             safeCell.chart.rightAxis.drawAxisLineEnabled = false
