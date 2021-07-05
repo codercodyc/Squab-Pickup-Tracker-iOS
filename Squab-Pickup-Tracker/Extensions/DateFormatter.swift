f340e93639c24e3fecf3e307c8f34aebd7838165
Cody Crawmer <codercodyc@gmail.com>
2021-07-05 13:56:50 -0700

    Transfer Posts work now

---
+++ Squab-Pickup-Tracker/Extensions/DateFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  DateFormatter.swift
+//  Squab-Pickup-Tracker
+//
+//  Created by Cody Crawmer on 7/5/21.
+//
+
+import Foundation
---
+++ Squab-Pickup-Tracker/Model/TransferData.swift
@@ -0,0 +1,8 @@
+//
+//  TransferData.swift
+//  Squab-Pickup-Tracker
+//
+//  Created by Cody Crawmer on 7/5/21.
+//
+
+import Foundation
